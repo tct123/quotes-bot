0f72b50515a68d9a9053936e72f074d7a7a8e57f
tct123 <42028373+tct123@users.noreply.github.com>
2023-02-19 09:52:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from data import *
 
 class Twitterbot():
     def __init__(self):
-        t = Twitter(auth=OAuth(TOKEN,TOKENSECRET,CONSUMERKEY,CONSUMERSECRET))
+        #Mastodon
+        t = Twitter(auth=OAuth(TOKEN,TOKENSECRET,CONSUMERKEY,CONSUMERSECRET)) #Twitter
         content = quotes.getQuotes(self, category="happiness", api_key="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk")
         print(content)
         t.statuses.update(
