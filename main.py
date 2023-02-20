dcb110d2218382247ec3cda3f79e3301a64b840f
tct123 <42028373+tct123@users.noreply.github.com>
2023-02-20 23:27:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from twitter import *
 from mastodon import *
-from quotesgenerator import *
+from quotesgenerator.main import *
 from data import *
 
 class Twitterbot():
     def __init__(self):
         #Mastodon
         t = Twitter(auth=OAuth(TOKEN,TOKENSECRET,CONSUMERKEY,CONSUMERSECRET)) #Twitter
-        content = quotesgenerator.getQuotes(self, category="happiness", api_key="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk")
+        content = getQuotes(self, category="happiness", api_key="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk")
         print(content)
         t.statuses.update(
             status = content)
