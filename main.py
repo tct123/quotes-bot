85e1255f607b273cb98ea714ba8b4c7a917ce2d6
tct123 <42028373+tct123@users.noreply.github.com>
2023-02-21 00:43:04 +0100

    .

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ import requests  # Will be replaced
 import json  # Will be replaced
 
 
-class Twitterbot():
+class Socialbot():
     def __init__(self):
+        api_key="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk"
+        category="happiness"
         # Mastodon
-        t = Twitter(auth=OAuth(TOKEN, TOKENSECRET,
-                    CONSUMERKEY, CONSUMERSECRET))  # Twitter
-        content = getQuotes(self, category="happiness",
-                            api_key="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk")
-        print(content)
-        t.statuses.update(
-            status=content)
-        print(content)
+        Mastodon.create_app(
+            'InspiringQuotes',
+            api_base_url = 'https://mastodon.social',
+            to_file = 'pytooter_clientcred.secret'
+        )
 
-    def getQuotes(self, category, api_key):
+        # t = Twitter(auth=OAuth(TOKEN, TOKENSECRET, CONSUMERKEY, CONSUMERSECRET))  # Twitter
         api_url = f'https://api.api-ninjas.com/v1/quotes?category={category}'
         response = requests.get(api_url, headers={'X-Api-Key': api_key})
         if response.status_code == requests.codes.ok:
@@ -26,9 +25,13 @@ class Twitterbot():
             quote = data[0]["quote"]
             author = data[0]["author"]
             content = f"{quote}\n\n{author}"
-            return content
         else:
             print("Error:", response.status_code, response.text)
 
 
-Twitterbot()
+        print(content)
+        # t.statuses.update(status=content)
+
+    def getQuotes(self, category, api_key):
+       pass
+Socialbot()
