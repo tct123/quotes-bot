1584a813d79675b6666848e429d2504af4d83edb
tct123 <42028373+tct123@users.noreply.github.com>
2023-02-21 00:46:34 +0100

    Update data.py

--- data.py
+++ data.py
@@ -2,3 +2,5 @@ CONSUMERKEY = "PmTDaCkhuBHOZxqAL8h72VBaK" #API Key
 CONSUMERSECRET = "3Kw8MKEu5DPY6PM9X43czxAhbZ6jQZw6mrr97VpBU11Axk2jgk" #API Key Secret
 TOKEN = "1613120183379394560-h9Y9qo44DoPMe8T4SzNnuD3brJe0FI" #Access token
 TOKENSECRET = "YGw5hvZ8w8BpueUJ1lu3u8gzuBub7Od9TT8t5c6MxOcqp" #Access Token Secret
+mastodon_email = "t-conze@web.de"
+mastodon_password = "mZLVmL%^zRnol!2W"
