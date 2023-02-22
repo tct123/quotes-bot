8bfe4ebd912f8f0014113c410508c5a3bd79a62b
tct123 <42028373+tct123@users.noreply.github.com>
2023-02-22 23:59:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ import time
 
 class Socialbot():
     def __init__(self):
-        api_key="As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk"
-        category="happiness"
+        api_key = "As3XyZ3Xl3IEUqpyUHpa6A==phjO5S4DqLxtFeqk"
+        category = "happiness"
         # Mastodon
         Mastodon.create_app(
             'InspiringQuotes',
-            api_base_url = 'https://mastodon.social',
-            to_file = 'pytooter_clientcred.secret'
+            api_base_url='https://mastodon.social',
+            to_file='pytooter_clientcred.secret'
         )
-        mastodon = Mastodon(client_id = 'pytooter_clientcred.secret',)
+        mastodon = Mastodon(client_id='pytooter_clientcred.secret',)
         mastodon.log_in(
             f'{mastodon_email}',
             f'{mastodon_password}',
-            to_file = 'pytooter_usercred.secret')
-        mastodon = Mastodon(access_token = 'pytooter_usercred.secret')
+            to_file='pytooter_usercred.secret')
+        mastodon = Mastodon(access_token='pytooter_usercred.secret')
         # t = Twitter(auth=OAuth(TOKEN, TOKENSECRET, CONSUMERKEY, CONSUMERSECRET))  # Twitter
         api_url = f'https://api.api-ninjas.com/v1/quotes?category={category}'
         response = requests.get(api_url, headers={'X-Api-Key': api_key})
@@ -31,16 +31,17 @@ class Socialbot():
             quote = data[0]["quote"]
             author = data[0]["author"]
             content = f"{quote}\n\n{author}"
+            print(content)
+            mastodon.toot(content)
         else:
             print("Error:", response.status_code, response.text)
 
-
-        print(content)
-        mastodon.toot(content)
         # t.statuses.update(status=content)
 
     def getQuotes(self, category, api_key):
-       pass
+        pass
+
+
 while True:
     Socialbot()
     time.sleep(3600)
