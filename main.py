bd55252a5f27fbe9e136d2a37664959d08715d24
tct123 <42028373+tct123@users.noreply.github.com>
2024-12-21 10:50:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,16 @@ class Socialbot:
         # Mastodon
         mastodon_email = os.getenv("MASTODON_EMAIL")
         mastodon_password = os.getenv("MASTODON_PASSWORD")
-        Mastodon.create_app(
-            "InspiringQuotes",
-            api_base_url="https://mastodon.social",
-            to_file="pytooter_clientcred.secret",
-        )
-        mastodon = Mastodon(
-            client_id="pytooter_clientcred.secret",
-        )
+        if not os.path.exists("pytooter_clientcred.secret"):
+            Mastodon.create_app(
+                "pytooterapp",
+                api_base_url="https://mastodon.social",
+                to_file="pytooter_clientcred.secret",
+            )
+        mastodon = Mastodon(client_id="pytooter_clientcred.secret")
         mastodon.log_in(
-            username=f"{mastodon_email}",
-            password=f"{mastodon_password}",
+            username=mastodon_email,
+            password=mastodon_password,
             to_file="pytooter_usercred.secret",
         )
         mastodon = Mastodon(access_token="pytooter_usercred.secret")
